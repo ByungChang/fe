b9936fe752eb7b69d39cc4b1f2ecf9a99b500996
unknown <byungchang@github.com>
2019-10-29 14:08:10 +0900

    하이퍼비전 확장 리스트 추가

--- CCMS_Dev/be/fe/src/components/userComponents/userDetail.vue
+++ CCMS_Dev/be/fe/src/components/userComponents/userDetail.vue
@@ -9,54 +9,68 @@
           <v-spacer></v-spacer>
           <v-btn 
           @click="infoClose()"
-      icon
-      dark>
+          icon
+          dark>
         <v-icon 
         size="30px">mdi-close</v-icon>
       </v-btn>
           </v-app-bar>
+          
           <v-card-text class="pa-0">
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/lists/ali.png"
-      height="200px"
-      dark
-    >
-    </v-img>
-<v-list v-if="userAccess">
-      <v-list-item   v-for="(item, i) in lists1" 
-        :key="i"
-      >
+            <v-img
+          src="@/assets/korea.png"
+          height="150px"
+          dark
+          >
+          </v-img>
+<v-list v-if="userAccess" two-line>
+      <v-list-item  
+       v-for="(item, i) in lists1" 
+       :key="i" >
         <v-list-item-icon>
           <v-icon color="indigo">{{item.icon}}</v-icon>
         </v-list-item-icon>
-        
         <v-list-item-content>
-          <v-list-item-title >{{ item.title }}</v-list-item-title>
-          
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{item.titleHead}}</v-list-item-subtitle> 
         </v-list-item-content>
-      </v-list-item>
-<v-divider inset></v-divider>
-</v-list>
+      </v-list-item> 
 
-<v-list v-else>
+        <v-list-group
+        color="indigo"
+        
+      >
+        <template v-slot:activator>
+          <v-list-item-icon>
+            <v-icon color="indigo">mdi-help</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>HV title을 가져와야함</v-list-item-title>
+            <v-list-item-subtitle>하이퍼비전 목록</v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="(item, i) in hvlists"
+          :key="i"
+          @click=""
+        >
+          <v-list-item-content>
+            <v-list-item-title style="text-align:right" v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+</v-list>
+<!--<v-list v-else>
       <v-list-item v-for="(item, i) in lists2" 
-        :key="i"
-      >
-        <v-list-item-icon>
-          <v-icon color="indigo">{{item.icon}}</v-icon>
-        </v-list-item-icon>
-        
+        :key="i">
         <v-list-item-content>
           <v-list-item-title >{{ item.title }}</v-list-item-title>
-          
         </v-list-item-content>
       </v-list-item>
 <v-divider inset></v-divider>
-</v-list>
-
+</v-list>-->
           </v-card-text>
-
-    
   </v-card>
 </v-dialog>
 </template>
@@ -66,19 +80,23 @@ import { EventBus } from "./eventBus";
 export default {
     data(){
         return{
-          formTitle:'',
+         hvlists:[
+           'hv-11111', 'hv-2', 'hv-3', 'hv-4', 'hv-5', 'hv-6', 'hv-7', 'hv-8'
+         ],
+
+         formTitle:'',
          userAccess:true, 
          dialog:false,
          lists1: 
             [
-        { title: '', icon: 'mdi-account-check',},
-        { title: '', icon: 'mdi-lock', },
-        { title: '', icon: 'mdi-antenna', },
-        { title: '', icon: 'mdi-calendar',},
-        { title: '', icon: 'mdi-phone', },
-        { title: '', icon: 'mdi-email', },
-        { title: '', icon: 'mdi-map-marker',},
+        { title: '', icon: 'mdi-account-check', titleHead:'사용자'},
+        { title: '', icon: 'mdi-lock',titleHead:'활성화 상태' },
+        { title: '', icon: 'mdi-calendar',titleHead:'계약 만료일 '},
+        { title: '', icon: 'mdi-phone',titleHead:'연락처' },
+        { title: '', icon: 'mdi-email',titleHead:'이메일' },
+        { title: '', icon: 'mdi-map-marker',titleHead:'주소'},
            ],
+
 
          lists2: 
             [
@@ -104,24 +122,24 @@ export default {
           this.lists1[0].title=item.userName   
           this.lists1[1].title=item.status 
           //this.lists[2].title=item.hv
-          this.lists1[3].title=item.endDay  
-          this.lists1[4].title=item.Tel
-          this.lists1[5].title=item.userMail
-          this.lists1[6].title=item.userAdress
+          this.lists1[2].title=item.endDay  
+          this.lists1[3].title=item.Tel
+          this.lists1[4].title=item.userMail
+          this.lists1[5].title=item.userAdress
           
           this.formTitle=item.userName
           this.dialog=true
           this.userAccess=true
     });
     EventBus.$on("companyDetail",(item) => {
-          this.lists2[0].title=item.name   
+          this.lists2[0].title=item.comName   
           this.lists2[1].title=item.status 
           //this.lists[2].title=item.hv
-          this.lists2[3].title=item.expiredDate  
-          this.lists2[4].title=item.comEmail  
-          this.lists2[5].title=item.businessNum  
+          this.lists2[2].title=item.endDay  
+          this.lists2[3].title=item.comEmail  
+          this.lists2[4].title=item.businessNum  
 
-          this.formTitle=item.name
+          this.formTitle=item.comName
 
           this.dialog=true
           this.userAccess=false
--- CCMS_Dev/be/fe/src/views/company.vue
+++ CCMS_Dev/be/fe/src/views/company.vue
@@ -158,6 +158,7 @@
       });
 
 
+
     },
     methods:{
       btnStatus(){
